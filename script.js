9253bc367604b40e524838271e33e85ca3e36f83
akbo0001 <akbo0001.algonquinlive.com>
2020-01-10 23:17:29 -0500

    Edit

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
-const name = "Cihan Akbolat"
-console.log(`Hello, my name is Your ${name}`);
+const name = "Cihan Akbolat";
+console.log(`Hello, my name is ${name}`);
 
-const positiveNum = 5
-
+const positiveNum = 5;
+console.log(const step1 = `2*${positiveNum}`
+const step2 = `5*${step1}`
+const step3 = `${step2}/${positiveNum}`
+  `${step3}-7`);
