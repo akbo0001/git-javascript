1a2db28dc6c300102a1120fddf9132d2d7aaeb9c
akbo0001 <akbo0001.algonquinlive.com>
2020-01-11 22:09:30 -0500

    Adds script file

--- script.js
+++ script.js
@@ -1,8 +1,14 @@
 const name = "Cihan Akbolat";
 console.log(`Hello, my name is ${name}`);
 
-const positiveNum = 5;
-console.log(const step1 = `2*${positiveNum}`
-const step2 = `5*${step1}`
-const step3 = `${step2}/${positiveNum}`
-  `${step3}-7`);
+const positiveNum = 3;
+const firstNum = 2;
+const secondNum = 5;
+const lastNum = 7;
+
+const step1 = positiveNum * firstNum;
+const step2 = step1 * secondNum;
+const step3 = step2 / positiveNum;
+const finalStep = step3 - lastNum;
+
+console.log(`${finalStep}`);
